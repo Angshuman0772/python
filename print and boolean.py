d6e524657338a792ec28572ae61a0ab49b8dd6d9
Angshuman0772 <77878555+Angshuman0772@users.noreply.github.com>
2025-01-18 00:11:40 +0530

    first commit

---
+++ print and boolean.py
@@ -0,0 +1,18 @@
+print("Hello")
+print('o-----')
+print(' ||||')
+print('*' * 10)
+
+price = 10
+rating = 4.9
+name = 'Angshuman'
+is_published = False # Boolean
+print(price)
+
+
+name = 'John Smith'
+age = 20
+is_new_patient = True
+print(name)
+print(age)
+print(is_new_patient)
